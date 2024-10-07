25fe7cce606ccf6555215d8304d35fe2100c3945
Frostleaft07 <zx7unknow@gmail.com>
2024-10-07 08:37:23 +0000

    copy theme
    Signed-off-by: Frostleaft07 <zx7unknow@gmail.com>

--- RMX2185-vendor.mk
+++ RMX2185-vendor.mk
@@ -18,7 +18,31 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX2185/proprietary/product/vendor_overlay/29/lib/libDiracAPI_SHARED.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libDiracAPI_SHARED.so \
     vendor/realme/RMX2185/proprietary/product/vendor_overlay/29/lib/libdpframework.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libdpframework.so \
     vendor/realme/RMX2185/proprietary/product/vendor_overlay/29/lib/soundfx/libdirac.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/soundfx/libdirac.so \
-    vendor/realme/RMX2185/proprietary/product/vendor_overlay/29/lib64/libdpframework.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib64/libdpframework.so
+    vendor/realme/RMX2185/proprietary/product/vendor_overlay/29/lib64/libdpframework.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib64/libdpframework.so \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Cupid_Pink:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Cupid_Pink \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Cupid_Pink.zip:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Cupid_Pink.zip \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Lavender:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Lavender \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Lavender.zip:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Lavender.zip \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Mauve_Purple:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Mauve_Purple \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Mauve_Purple.zip:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Mauve_Purple.zip \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Medium_Aquamarine:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Medium_Aquamarine \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Medium_Aquamarine.zip:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Medium_Aquamarine.zip \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Medium_Turquoise:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Medium_Turquoise \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Medium_Turquoise.zip:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Medium_Turquoise.zip \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Pale_Cornflower_Blue:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Pale_Cornflower_Blue \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Pale_Cornflower_Blue.zip:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Pale_Cornflower_Blue.zip \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Rajah_Orange:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Rajah_Orange \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Rajah_Orange.zip:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Rajah_Orange.zip \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Sky_Blue:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Sky_Blue \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Sky_Blue.zip:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Sky_Blue.zip \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Sundown:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Sundown \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Sundown.zip:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Sundown.zip \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Vivid_Yellow:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Vivid_Yellow \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Vivid_Yellow.zip:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Vivid_Yellow.zip \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Wild_Willow:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Wild_Willow \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Wild_Willow.zip:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Wild_Willow.zip \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Oigiri_Board:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Oigiri_Board \
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Oigiri_Board.zip:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Oigiri_Board.zip
 
 PRODUCT_PACKAGES += \
     EngineerMode \
