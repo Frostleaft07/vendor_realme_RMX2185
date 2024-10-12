7b0681fcbf6089022eab93ccf33420c9b5bb239c
Frostleaft07 <zx7unknow@gmail.com>
2024-10-12 22:45:26 +0000

    Import fp binary
    Signed-off-by: Frostleaft07 <zx7unknow@gmail.com>

--- RMX2185-vendor.mk
+++ RMX2185-vendor.mk
@@ -1442,7 +1442,11 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Wild_Willow:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Wild_Willow \
     vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Wild_Willow.zip:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Wild_Willow.zip \
     vendor/realme/RMX2185/proprietary/etc/gboard_theme/Oigiri_Board:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Oigiri_Board \
-    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Oigiri_Board.zip:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Oigiri_Board.zip
+    vendor/realme/RMX2185/proprietary/etc/gboard_theme/Oigiri_Board.zip:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Oigiri_Board.zip \
+    vendor/realme/RMX2185/proprietary/etc/default-permissions/default-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/default-permissions.xml \
+    vendor/realme/RMX2185/proprietary/etc/permissions/privapp-permissions-op-launcher.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-op-launcher.xml \
+    vendor/realme/RMX2185/proprietary/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/op-launcher-hiddenapi-package-whitelist.xml \
+    vendor/realme/RMX2185/proprietary/product/vendor_overlay/29/bin/hw/vendor.oppo.hardware.biometrics.fingerprint@2.1-service:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/bin/hw/vendor.oppo.hardware.biometrics.fingerprint@2.1-service
 
 PRODUCT_PACKAGES += \
     EngineerMode \
