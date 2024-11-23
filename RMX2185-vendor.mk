f7d4e5214a0c784b90dbcd7b68ff6043a062b868
Frostleaft07 <zx7unknow@gmail.com>
2024-11-24 05:19:18 +0700

    lib: hapus libdpframework.so
    Signed-off-by: Frostleaft07 <zx7unknow@gmail.com>

--- RMX2185-vendor.mk
+++ RMX2185-vendor.mk
@@ -14,8 +14,6 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX2185/proprietary/lib64/libaptXHD_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptXHD_encoder.so \
     vendor/realme/RMX2185/proprietary/lib64/libaptX_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptX_encoder.so \
     vendor/realme/RMX2185/proprietary/lib64/libem_support_jni.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libem_support_jni.so \
-    vendor/realme/RMX2185/proprietary/product/vendor_overlay/29/lib/libdpframework.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libdpframework.so \
-    vendor/realme/RMX2185/proprietary/product/vendor_overlay/29/lib64/libdpframework.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib64/libdpframework.so \
     vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Cupid_Pink:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Cupid_Pink \
     vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Cupid_Pink.zip:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Cupid_Pink.zip \
     vendor/realme/RMX2185/proprietary/etc/gboard_theme/Light_Lavender:$(TARGET_COPY_OUT_SYSTEM)/etc/gboard_theme/Light_Lavender \
